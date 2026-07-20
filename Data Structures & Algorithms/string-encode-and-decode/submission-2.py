13f9b6fe6f4baaca52d2cd6e369f42c8a6fc1813
Zachary Kelly <zjkelly19@gmail.com>
2026-07-20 13:43:42 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # First, let's encode all the strs to hex. This ensures 
+        # that there will be no issues using character delimiters 
+        for idx, s in enumerate(strs):
+            strs[idx] = s.encode("utf-8").hex() 
+
+        # Then join with a delimiter
+        return "#".join(strs) if len(strs) > 0 else "EMPTY"
+
+    def decode(self, s: str) -> List[str]:
+        print(f"THIS IS THE DECODED STRING: {s}")
+        if(s == "EMPTY"):
+            return []
+
+        # Split on the delimiter 
+        tokenized: list[str] = s.split("#")
+
+        # decode each of the strs 
+        for idx, str_ in enumerate(tokenized):
+            tokenized[idx] = bytes.fromhex(str_).decode("utf-8")
+        
+        return tokenized
+        
